b7f2b09147bd71f6071536c05537071d9b77f736
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-06-07 18:08:59 +0600

    Add functionalities for add remove and edit todo list and fixed linters errors

--- src/index.js
+++ src/index.js
@@ -1,62 +1,19 @@
 /* eslint max-classes-per-file: "off" */
 import _ from 'lodash';
 import './index.css';
+import ToDoList from './module/todo.js';
 
-class Task {
-  constructor(description, completed, index) {
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
+const todoList = new ToDoList();
+
+const inputTodo = document.getElementById('input-todo');
+inputTodo.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const description = inputTodo.value;
+    if (description) {
+      todoList.addTask(description);
+      inputTodo.value = '';
+      todoList.render();
+    }
   }
-}
-
-const tasks = [
-  new Task('Task 1', false, 1),
-  new Task('Task 2', true, 2),
-  new Task('Task 3', false, 3),
-];
-
-class ToDoList {
-  constructor(tasks) {
-    this.tasks = tasks;
-  }
-
-  render() {
-    const todoList = document.getElementById('todo-list');
-
-    this.tasks.forEach((task) => {
-      const listItem = document.createElement('div');
-      listItem.classList.add('item');
-      const div = document.createElement('div');
-      div.classList.add('internal-div');
-      const inputItem = document.createElement('input');
-      inputItem.classList.add('checkbox');
-      inputItem.type = 'checkbox';
-      if (task.checked) {
-        listItem.classList.add('completed');
-      }
-      const itemDescription = document.createElement('p');
-      itemDescription.innerText = task.description;
-      div.appendChild(itemDescription);
-      listItem.appendChild(inputItem);
-      listItem.appendChild(div);
-
-      const threeDots = document.createElement('i');
-      threeDots.className = 'fa-solid fa-ellipsis-vertical';
-      threeDots.classList.add('threedots');
-      div.appendChild(threeDots);
-      listItem.appendChild(div);
-      todoList.appendChild(listItem);
-    });
-    const clearAll = document.createElement('div');
-    clearAll.classList.add('clear-all');
-    const clearBtn = document.createElement('button');
-    clearBtn.classList.add('clear-btn');
-    clearBtn.innerText = 'Clear all completed';
-    clearAll.appendChild(clearBtn);
-    todoList.appendChild(clearAll);
-  }
-}
-
-const todoList = new ToDoList(tasks);
+});
 todoList.render();
