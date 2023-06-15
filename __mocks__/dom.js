8e3e05bdfa72779f5ea383e875c35170d5ee1baf
Abdoul <abdoulramanediallo44@gmail.com>
2023-06-15 09:04:44 +0000

    new update in the dom

--- __mocks__/dom.js
+++ __mocks__/dom.js
@@ -2,21 +2,28 @@
 /* eslint class-methods-use-this: "off" */
 class MockElement {
   addEventListener() {}
-
   removeEventListener() {}
   // Add other methods and properties as needed
 }
 
 class MockDocument {
-  createElement() {
+  createElement(tagName) {
     return new MockElement();
+  }
+
+  getElementById(id) {
+    const element = new MockElement();
+    if (id === 'todo-list') {
+      element.id = id;
+    }
+    return element;
   }
   // Add other methods and properties as needed
 }
 
 class MockWindow {
-    document = new MockDocument();
-    // Add other methods and properties as needed
+  document = new MockDocument();
+  // Add other methods and properties as needed
 }
 
 const mockWindow = new MockWindow();
